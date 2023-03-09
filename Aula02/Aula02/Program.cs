4b03880dda83b25428e54a9dd1b2d7180d6c6acf
Douglas Legramante <doug-leg@hotmail.com>
2023-03-09 09:47:04 -0400

    Adicionado projeto Aula02

---
+++ Aula02/Aula02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercício 01
+
+            Console.WriteLine("\t\t---Dados do usuário---\n");
+            string nome, endereco, telefone;
+
+            Console.Write("Nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Endereço: ");
+            endereco = Console.ReadLine();
+
+            Console.Write("Telefone: ");
+            telefone = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("\t\t---Dados do usuário---\n");
+            Console.WriteLine("Nome: " + nome + "\nEndereço: " + endereco + "\nTelefone: " + telefone);
+            Console.WriteLine("\n\t\t---Dados do usuário---\n");
+            Console.WriteLine("Nome: {0}\nEndereço: {1}\nTelefone: {2}", nome,endereco,telefone);
+            Console.WriteLine("\n\t\t---Dados do usuário---\n");
+            Console.WriteLine($"Nome: {nome}\nEndereço: {endereco}\nTelefone: {telefone}");
+
+        }
+    }
+}
---
+++ Aula02/Exercicio01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0; //2147483647 valor máximo suportado pelo tipo int
+            int b = 0; //-2147483648 valor minímo suportado pelo tipo int
+            int c = 1;
+            //Console.WriteLine(a / b);
+            Console.WriteLine("Máximo valor possivel: " + a);
+            Console.WriteLine("Overflow: " + (a + 2)); // overflow -2147483648 +2
+            Console.WriteLine("Minimo valor possivel: " + b);
+            Console.WriteLine("Underflow: " + (b - 2)); // underflow 2147483647
+
+            Console.WriteLine(double.MaxValue * c); // overflow: infinity (8)
+            Console.WriteLine(double.MinValue * c); // overflow: -infinity (-8)
+            Console.WriteLine(double.Epsilon / c); // underflow: 0.0
+            Console.WriteLine(b/a);
+
+            Console.ReadKey();
+        }
+    }
+}
